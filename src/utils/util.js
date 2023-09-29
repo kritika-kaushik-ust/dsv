13b58771fe44caa8d6b976cf428a4f57596fca84
kritika-kaushik-ust <kritika.kaushik@ust.com>
2023-09-29 10:08:29 +0530

    added company name sorting

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,6 +1,9 @@
 const sortByProperty = (prop, order = 1) => {
     if (order !== -1) order = 1;
     return (a, b) => {
+    if(prop == 'company')
+       return (a[prop]['name'] > b[prop]['name']) ? 1: (a[prop]['name'] < b[prop]['name']) ? -1: 0;
+    else
       return (a[prop] > b[prop]) ? 1: (a[prop] < b[prop]) ? -1: 0;
     };
   };
